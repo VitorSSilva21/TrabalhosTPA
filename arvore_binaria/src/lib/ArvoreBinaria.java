6a44abd942a39d83e736bf458c62b7be69d25936
Valzer <57049102+ValzerBR@users.noreply.github.com>
2023-10-06 08:39:22 -0300

    Update ArvoreBinaria.java

--- arvore_binaria/src/lib/ArvoreBinaria.java
+++ arvore_binaria/src/lib/ArvoreBinaria.java
@@ -111,8 +111,33 @@ public class ArvoreBinaria<T> implements IArvoreBinaria<T> {
 
     @Override
     public String caminharEmNivel() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.    
+    if (raiz == null) {
+        return "[]";
     }
+
+    Queue<No<T>> fila = new LinkedList<>();
+    fila.offer(raiz);
+
+    StringBuilder resultado = new StringBuilder("[");
+    while (!fila.isEmpty()) {
+        No<T> no = fila.poll();
+        resultado.append(no.getChave());
+
+        if (no.getFilhoEsquerda() != null) {
+            fila.offer(no.getFilhoEsquerda());
+        }
+        if (no.getFilhoDireita() != null) {
+            fila.offer(no.getFilhoDireita());
+        }
+
+        if (!fila.isEmpty()) {
+            resultado.append(", ");
+        }
+    }
+    resultado.append("]");
+
+    return resultado.toString();
+}
 
 //CAMINHA EM ORDEM
     private String _caminharEmOrdem(T r) {
