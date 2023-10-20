6d70dcd0b138962592e83fad7ef06e57a4925aef
Lawrence_ARA <88056418+LorhanSouza@users.noreply.github.com>
2023-10-20 09:04:03 -0300

    Add files via upload

--- arvore_binaria/src/lib/ArvoreAVL.java
+++ arvore_binaria/src/lib/ArvoreAVL.java
@@ -1,11 +1,25 @@
 package lib;
 
 import java.util.Comparator;
+import src.lib.ArvoreBinaria;
 
 public class ArvoreAVL <T> extends ArvoreBinaria<T>{
 
     public ArvoreAVL(Comparator<T> comparator) {
         super(comparator);
+    }
+
+    /**
+     *
+     * @param No raiz da subarvore com elemento do tipo T armazenado.
+     * @return No<T> com a rotação feita no trecho da árvore concluída
+     */
+    private No<T> rotacaoEsquerda(No<T> raiz){
+        No<T> rotacionado = raiz.getFilhoDireita();
+        raiz.setFilhoDireita(rotacionado.getFilhoEsquerda());
+        rotacionado.setFilhoEsquerda(raiz);
+
+        return rotacionado;
     }
     
     //Implementar métodos para efetuar o balanceamento e sobrescrever método de adicionar elemento...
